08c42e242469399e729b576dab0f0883ed32ed1b
NellybettIrahola <nellybett.irahola@gmail.com>
2019-07-29 01:57:37 -0400

    Remove bad folder

--- ChampernowneCalculator/app/src/main/java/com/example/champernownecalculator/model/Number.java
+++
@@ -1,8 +0,0 @@
-package com.example.champernownecalculator.model;
-
-public interface Number {
-	abstract int getPatternPosition(String pattern, String number);
-	abstract String calculate(int decimals, double base);
-	abstract String calculateAlternative(int decimals, int base);
-	
-}
